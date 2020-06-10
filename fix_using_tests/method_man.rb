41eacf17ea8740deccca658e4a80c8fd6f34260c
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 20:59:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,9 @@
 # don't forget to add: require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{playar2}"
+  p "Hello #{player1} & #{playar2}"
 end
 
 def play_game(player1, player2)
-  puts "#{player1} is better than #{player2}"
+  p "#{player1} is better than #{player2}"
 end
