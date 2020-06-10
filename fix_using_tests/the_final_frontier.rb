c7fcae7c8a892715e309606337b6c6b3d7095f85
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 21:06:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def generate_star_date
   star_date = (rand(100000) + 400000) / 10.0
   star_date
@@ -13,6 +13,4 @@ end
 def engage()
   date = generate_star_date
   puts state_log(date)
-  binding.pry
-  puts "hello"
 end
