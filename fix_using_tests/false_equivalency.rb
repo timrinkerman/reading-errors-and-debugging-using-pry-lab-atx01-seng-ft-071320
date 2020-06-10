05fd6d81a6683e419aeb904396b9ef1b122700bb
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 20:25:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,10 +11,10 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  if num = 1.to_s
      binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num = 2.to_s
      binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
