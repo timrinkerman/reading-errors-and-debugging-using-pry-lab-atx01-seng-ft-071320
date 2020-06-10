e469eb02bdbf3aa53c04a2fecf8396bd04950214
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 20:55:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,10 @@ end
 
 def selection(num)
   if num == 1
-      puts "YUM YUM MUNCH MUNCH MUNCH"
+      p "YUM YUM MUNCH MUNCH MUNCH"
   binding.pry
   elsif num == 2
-     puts "HAM HAM HAM IN MY TUMMY"
+     p "HAM HAM HAM IN MY TUMMY"
   end
 end
 
