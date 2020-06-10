de8f16bfe8ca37fa25be82b9c236f7722063f3ec
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 21:05:11 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,10 @@
 # don't forget to add: require 'pry'
-
+require "pry"
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
+  star_date
+  binding.pry
+  puts "hello"
 end
 
 
@@ -9,7 +12,8 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage
-  puts state_log(date)
+def engage()
   date = generate_star_date
+  puts state_log(date)
+  
 end
